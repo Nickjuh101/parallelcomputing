ecdb5ac29f25409be942f50727c696fa1d106e30
Nick <nick-schokker@hotmail.com>
2018-05-16 14:52:47 +0200

    main and worker classes

---
+++ src/Worker.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Nick on 0016 16 mei 2018.
+ */
+public class Worker {
+
+    private Object lock1 = new Object();
+    private Object lock2 = new Object();
+
+    private String text;
+
+    
+
+}

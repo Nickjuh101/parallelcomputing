6b2009559ecd52a8345f35fd21c7d3df24816a0c
Nick <nick-schokker@hotmail.com>
2018-05-16 22:55:39 +0200

    Implemented serial solution to our problem

--- src/serialsolution/Main.java
+++ src/serialsolution/Main.java
@@ -1,19 +1,48 @@
 package serialsolution;
 
-import java.util.regex.Pattern;
+import java.util.Scanner;
 
 /**
  * Created by Nick on 0016 16 mei 2018.
  */
 public class Main {
 
-    private static int wordCount;
+    private static int wordsInText = 0;
+    private static int totalWordCount = 0;
+    private static String findWord;
     private static String text;
 
     public static void main(String[] args) {
-        for (int i = 0; i < text.length(); i++){
-            // TODO Pattern.matches
+        Scanner scan = new Scanner(System.in);
+
+        /* Ask for a text */
+        System.out.println("Please enter a text you want to find a word in: ");
+        text = scan.nextLine();
+
+        /* Remove punctuation from the text, except of punctuation that is useful for certain words.
+         * Examples of these words are don't or re-enter */
+        text = text.replaceAll("[[\\W]&&[^']&&[^-]]", " ");
+
+        /* Replace all double whitespaces with single whitespaces.
+         * We will split the text on these whitespaces later */
+        text = text.replaceAll("\\s\\s+"," ");
+
+        /* Ask for a word to find in the text */
+        System.out.println("Please enter the word you want to count in the text: ");
+        findWord = scan.nextLine();
+
+        System.out.println(text);
+        /* Count total words in the text, count the amount of times the given word is in the text */
+        for (String element : text.split(" ")){
+            totalWordCount++;
+            if (element.equalsIgnoreCase(findWord)){
+                wordsInText++;
+            }
         }
+
+        /* Print the useful lines of the algorithm */
+        System.out.println("The total word count in the text is: " + totalWordCount);
+        System.out.println("The word " + findWord + " appears " + wordsInText + " times in the given text");
     }
 
 }

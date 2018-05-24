dddd8fc42b77772caf42ba9866b98c9fa84efaf7
Nick <nick-schokker@hotmail.com>
2018-05-24 10:13:24 +0200

    Implemented serial

--- src/serialsolution/Main.java
+++ src/serialsolution/Main.java
@@ -1,10 +1,8 @@
 package serialsolution;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import org.apache.commons.lang.time.StopWatch;
+
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -17,44 +15,45 @@ public class Main {
     private static String findWord;
     private static String text;
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+//        Scanner scan = new Scanner(new File("src/text.txt"));
+        Scanner findword = new Scanner(System.in);
 
-        try {
-            text = readFile("C:\\Users\\Nick\\IdeaProjects\\parallelcomputing\\src\\text.txt", StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        long startTime = System.currentTimeMillis();
 
+        FileReader file = new FileReader("src/testText.txt");
+        BufferedReader br = new BufferedReader(file);
 
-        /* Remove punctuation from the text, except of punctuation that is useful for certain words.
-         * Examples of these words are don't or re-enter */
-        text = text.replaceAll("[[\\W]&&[^']&&[^-]]", " ");
-
-        /* Replace all double whitespaces with single whitespaces.
-         * We will split the text on these whitespaces later */
-        text = text.replaceAll("\\s\\s+"," ");
 
         /* Ask for a word to find in the text */
         System.out.println("Please enter the word you want to count in the text: ");
-        findWord = scan.nextLine();
+        findWord = findword.nextLine();
 
-        /* Count total words in the text, count the amount of times the given word is in the text */
-        for (String element : text.split(" ")){
-            totalWordCount++;
-            if (element.equalsIgnoreCase(findWord)){
-                wordsInText++;
+        while ((text = br.readLine()) != null) {
+        /* Remove punctuation from the text, except of punctuation that is useful for certain words.
+         * Examples of these words are don't or re-enter */
+            text = text.replaceAll("[[\\W]&&[^']&&[^-]]", " ");
+
+        /* Replace all double whitespaces with single whitespaces.
+         * We will split the text on these whitespaces later */
+            text = text.replaceAll("\\s\\s+", " ");
+
+            /* Count total words in the text, count the amount of times the given word is in the text */
+            for (String element : text.split(" ")) {
+                totalWordCount++;
+                if (element.equalsIgnoreCase(findWord)) {
+                    wordsInText++;
+                }
             }
         }
 
         /* Print the useful lines of the algorithm */
-        System.out.println("The total word count in the text is: " + totalWordCount);
-        System.out.println("The word " + findWord + " appears " + wordsInText + " times in the given text");
-    }
+            System.out.println("The total word count in the text is: " + totalWordCount);
+            System.out.println("The word " + findWord + " appears " + wordsInText + " times in the given text");
 
-    private static String readFile(String path, Charset encoding) throws IOException {
-        byte[] encoded = Files.readAllBytes(Paths.get(path));
-        return new String(encoded, encoding);
-    }
+        long stopTime = System.currentTimeMillis();
 
+        System.out.println("Elapsed time was " + (stopTime - startTime) + " miliseconds.");
+
+    }
 }
